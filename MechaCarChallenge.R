f67509b3ff10dcbe46756bc466b9868b32200432
lilydionne <dionne.johnson86@gmail.com>
2022-06-16 20:44:43 -0400

    Part 2 of Challenge

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -2,3 +2,9 @@ mpg_table <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F
 
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mpg_table)
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mpg_table))
+
+coil_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+total_summary <- coil_table %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+
+lot_summary <- coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep')
